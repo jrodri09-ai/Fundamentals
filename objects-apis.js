7f5da8d6a4cba771c03bc09bf9f25d11c915b7f8
Jessica Rodriquez <jrodriquez15@cnm.edu>
2021-03-31 23:39:36 -0600

    finished fundamentals

---
+++ objects-apis.js
@@ -0,0 +1,19 @@
+/*
+*Objects, Interfaces, and API's
+ */
+
+
+
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+
+
+let y = {
+    name: "Tom",
+    num: 10,
+};
+x(y);
+console.log(y);
